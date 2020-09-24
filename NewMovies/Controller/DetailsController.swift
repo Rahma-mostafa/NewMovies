4ba5cba089604c8cd13083d32fd9ff1a380be8fb
Rahma <mrahma057@gmail.com>
2020-09-24 10:38:12 +0200

    Run movies Trailer uploaded on YouTube

--- NewMovies/Controller/DetailsController.swift
+++ NewMovies/Controller/DetailsController.swift
@@ -10,8 +10,13 @@ import UIKit
 import Alamofire
 import SDWebImage
 import KRProgressHUD
+import SafariServices
 
 
+
+//protocol videoDelegate {
+//    func didTapWatchNow(url: String)
+//}
 class DetailsController: UIViewController {
     @IBOutlet var movieNameLabel: UILabel!
     @IBOutlet var movieCategoryLabel: UILabel!
@@ -20,13 +25,22 @@ class DetailsController: UIViewController {
     @IBOutlet var rateLabel: UILabel!
     @IBOutlet var descriptionLabel: UILabel!
     //variables
-    
-    var selectedRow:[Result] = []
+    var selectedRow:[Result] = [] //passed array that has the selected Item
     let baseImageUrl = "https://image.tmdb.org/t/p/original"
+    // I assume any static videoUrl for all items as the api used does't have videos url
+    let videoUrl = "https://www.youtube.com/watch?v=cjzu8pzx1Wc&t=1s&has_verified=1"
+
+//    var delegate: videoDelegate?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setCornerRadius()
         setData()
+//        delegate = self
+    }
+    func setCornerRadius(){
+        movieImageView.layer.cornerRadius = 32
+        videoImageView.layer.cornerRadius = 8
     }
     func setData(){
         self.movieNameLabel.text = selectedRow[0].originalTitle
@@ -39,29 +53,29 @@ class DetailsController: UIViewController {
         let videoImageUrl = selectedRow[0].backdropPath
         let ImageUrl = URL(string: "\(baseImageUrl)" + "\(videoImageUrl)")
         self.videoImageView.sd_setImage(with: ImageUrl)
-
-        
-
     }
-    func setCornerRadius(){
-        movieImageView.layer.cornerRadius = 32
-        videoImageView.layer.cornerRadius = 8
-    }
+    func didTapWatchNow(url: String){
+         let videoURL = URL(string: url)!
+         let safariVC = SFSafariViewController(url: videoURL)
+         self.present(safariVC, animated: true, completion: nil)
+     }
     
     @IBAction func onBackButtonTapped(_ sender: Any) {
     }
     @IBAction func onFavouriteButtonTapped(_ sender: Any) {
     }
     @IBAction func onPlayButtonTapped(_ sender: Any) {
-
+//        delegate?.didTapWatchNow(url: videoUrl)
+        didTapWatchNow(url: videoUrl)
     }
 
-}
 
-//protocol videoCellDelegate {
-//    func didTapWatchNow(url: String)
-//}
-//var delegate: videoCellDelegate?
+
+}
+ 
+
+
+
 
 
 
